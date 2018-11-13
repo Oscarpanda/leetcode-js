55d1c8970cc96e1ff74945f335cdb98bfc64b92b
gaoxiong.wu <wugaoxiong@100tal.com>
2018-11-13 20:06:20 +0800

    t

---
+++ js-points/promise.js
@@ -0,0 +1,9 @@
+let a = new Promise((res, rej) =>{
+   console.log(redafdsfsd);
+   res(9999999)
+   console.log('sdafsdf')
+}).then((res) => {
+  console.log(res)
+}).catch((err) => {
+console.log(err + "d")
+})
