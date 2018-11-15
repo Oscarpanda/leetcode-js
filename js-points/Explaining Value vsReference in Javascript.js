e67ef973630e6141956174af2ee4434d7e4155c8
Oscarpanda <wu_gaoxiong@163.com>
2018-11-15 21:56:27 +0800

    值传递引用传递

---
+++ js-points/Explaining Value vsReference in Javascript.js
@@ -0,0 +1,16 @@
+function changeAgeAndReference(person) {
+  person.age = 25;
+  person = {
+      name: 'John',
+      age: 50
+  };
+  
+  return person;
+}
+var personObj1 = {
+  name: 'Alex',
+  age: 30
+};
+var personObj2 = changeAgeAndReference(personObj1);
+console.log(JSON.stringify(personObj1)); // -> ?
+console.log(JSON.stringify(personObj2)); // -> ?
