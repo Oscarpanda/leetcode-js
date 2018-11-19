90fad74b14e51dc20776670a00e3ab3d56369841
Oscarpanda <wu_gaoxiong@163.com>
2018-11-19 11:44:43 +0800

    开启typescript

---
+++ typeScriptLearn/hellowTypeScript.js
@@ -0,0 +1,5 @@
+function sayHello(person) {
+    return 'Hello, ' + person;
+}
+var user = "1";
+console.log(sayHello(user));
---
+++ typeScriptLearn/hellowTypeScript.ts
@@ -0,0 +1,6 @@
+function sayHello(person: string) {
+  return 'Hello, ' + person;
+}
+
+let user = "1";
+console.log(sayHello(user));
