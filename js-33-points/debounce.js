cdf3803df91d1b4f3c81bfe9093e7a462d03b1db
Oscarpanda <wu_gaoxiong@163.com>
2019-03-03 01:23:39 +0800

    x

---
+++ js-33-points/debounce.js
@@ -0,0 +1,21 @@
+function throttle(fn) {
+    let canRun = true; // 通过闭包保存一个标记
+    return function () {
+        console.log(canRun)
+      if (!canRun) return; // 在函数开头判断标记是否为true，不为true则return
+      canRun = false; // 立即设置为false
+      setTimeout(() => { // 将外部传入的函数的执行放在setTimeout中
+        fn.apply(this, arguments);
+        // 最后在setTimeout执行完毕后再把标记设置为true(关键)表示可以执行下一次循环了。当定时器没有执行的时候标记永远是false，在开头被return掉
+        canRun = true;
+      }, 4500);
+    };
+}
+function sayHi() {
+    // console.log(this, "dd");
+}
+let fn = throttle(sayHi)
+setInterval(() => {
+    fn()
+}, 1000
+)
